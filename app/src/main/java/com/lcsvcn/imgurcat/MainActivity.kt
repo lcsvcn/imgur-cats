0efd3e5459beb72d9a0936c01552a01fe9d6751f
Lucas V C Nicolau <lcs.vcn@gmail.com>
2019-11-11 11:44:01 -0300

    working

--- app/src/main/java/com/lcsvcn/imgurcat/MainActivity.kt
+++ app/src/main/java/com/lcsvcn/imgurcat/MainActivity.kt
@@ -7,7 +7,6 @@ import android.widget.ImageView
 import okhttp3.*
 import java.io.IOException
 
-import com.squareup.picasso.Picasso
 import org.json.JSONObject
 import android.graphics.drawable.Drawable
 import java.io.InputStream
@@ -18,12 +17,33 @@ class MainActivity : AppCompatActivity() {
     private val client = OkHttpClient()
     private lateinit var responseJSON : JSONObject
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
+
 
         getCatImages("https://api.imgur.com/3/gallery/search/?q=cats")
+    }
+
+    fun getImageViews(size : Int) : Array<ImageView?> {
+        var imageViewArray : Array<ImageView?> = arrayOfNulls<ImageView>(size)
+
+        for(i in 0..size) {
+            try {
+                imageViewArray[i] = findViewById<ImageView>(
+                    resources.getIdentifier(
+                        "catImg$i", "id",
+                        packageName
+                    )
+                )
+            } catch (e: Exception) {
+                Log.d("DEV", "OPS $e")
+            }
+        }
+
+        return imageViewArray
     }
     
     // Handle Get Cat Images from Imgur
@@ -56,6 +76,8 @@ class MainActivity : AppCompatActivity() {
         val data = responseJSON
             .getJSONArray("data")
 
+        var imageNumber = 0
+
         val size = data.length() - 1
 
         for(i in 0..size) {
@@ -66,8 +88,10 @@ class MainActivity : AppCompatActivity() {
                     .getString("link")
 
               // avoid send gif or mp4
-              if(link.endsWith(".jpg"))
-                    showCatImages(link)
+              if(link.endsWith(".jpg")) {
+                  showCatImages(link, imageNumber)
+                  imageNumber++
+              }
 
             } catch(e : Exception) {
                 Log.d("DEV", "OPS $e")
@@ -77,14 +101,16 @@ class MainActivity : AppCompatActivity() {
 
     // Handle Show Cat Images
     // Improve
-    fun showCatImages(link : String) {
-        var imageView1 = findViewById<ImageView>(R.id.catImg1)
+    fun showCatImages(link : String, pos : Int) {
+
+
+        val imageViewArray = getImageViews(24)
 
         try {
             val myUrl = URL(link)
-            val inputStream = myUrl.getContent() as InputStream
+            val inputStream = myUrl.content as InputStream
             val drawable = Drawable.createFromStream(inputStream, null)
-            imageView1.setImageDrawable(drawable)
+            imageViewArray[pos]!!.setImageDrawable(drawable)
         } catch(e : Exception) {
             Log.d("DEV", "OPS $e")
         }
