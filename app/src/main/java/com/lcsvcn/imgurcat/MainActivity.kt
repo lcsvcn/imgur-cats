8bc29dd519aaf2f7130c1595754cec5e6a126fa1
Lucas V C Nicolau <lcs.vcn@gmail.com>
2019-11-11 12:52:31 -0300

    first mvp

--- app/src/main/java/com/lcsvcn/imgurcat/MainActivity.kt
+++ app/src/main/java/com/lcsvcn/imgurcat/MainActivity.kt
@@ -9,6 +9,10 @@ import java.io.IOException
 
 import org.json.JSONObject
 import android.graphics.drawable.Drawable
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.TextView
+import org.w3c.dom.Text
 import java.io.InputStream
 import java.net.URL
 
@@ -16,13 +20,19 @@ import java.net.URL
 class MainActivity : AppCompatActivity() {
     private val client = OkHttpClient()
     private lateinit var responseJSON : JSONObject
-
+    private lateinit var spinner : ProgressBar
+    private lateinit var spinnerText : TextView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+
+        spinner = findViewById(R.id.progressBar1)
+        spinnerText = findViewById(R.id.progressText1)
+        spinner.setVisibility(View.VISIBLE)
 
         getCatImages("https://api.imgur.com/3/gallery/search/?q=cats")
     }
@@ -69,6 +79,11 @@ class MainActivity : AppCompatActivity() {
         })
     }
 
+    fun givePercentage(pos : Int, size : Int) : Int {
+        var por = (pos * 100 / size)
+        Log.d("DEV", "porcentagem $por")
+        return (pos * 100 / size)
+    }
 
     // Handle Success response from API
     fun handleSuccessResponse(json : String) {
@@ -77,9 +92,9 @@ class MainActivity : AppCompatActivity() {
             .getJSONArray("data")
 
         var imageNumber = 0
+        var maxImages= 28
 
         val size = data.length() - 1
-
         for(i in 0..size) {
             try {
                 var link = data.getJSONObject(i)
@@ -89,31 +104,43 @@ class MainActivity : AppCompatActivity() {
 
               // avoid send gif or mp4
               if(link.endsWith(".jpg")) {
-                  showCatImages(link, imageNumber)
+                  showCatImages(link, imageNumber, maxImages)
                   imageNumber++
               }
+              if(imageNumber > maxImages) {
+                  break
+              }
 
+              var porcentage = givePercentage(imageNumber, maxImages)
+              spinner.setProgress(porcentage)
+              spinnerText.setText(porcentage.toString() + "%")
             } catch(e : Exception) {
-                Log.d("DEV", "OPS $e")
+                Log.d("DEV", "handleSuccessResponse : $e")
             }
         }
+
+        spinner.setVisibility(View.GONE)
+        spinnerText.setVisibility(View.GONE)
     }
 
     // Handle Show Cat Images
     // Improve
-    fun showCatImages(link : String, pos : Int) {
+    fun showCatImages(link : String, pos : Int, max : Int) {
 
 
-        val imageViewArray = getImageViews(24)
+        val imageViewArray = getImageViews(max)
 
         try {
             val myUrl = URL(link)
             val inputStream = myUrl.content as InputStream
             val drawable = Drawable.createFromStream(inputStream, null)
+
             imageViewArray[pos]!!.setImageDrawable(drawable)
         } catch(e : Exception) {
-            Log.d("DEV", "OPS $e")
+            Log.d("DEV", "showCatImages : $e")
         }
+
+
 
     }
 
